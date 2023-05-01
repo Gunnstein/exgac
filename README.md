7cba94f44a47233a35eef4d556a26d6af61af633
Gunnstein Thomas Frøseth <gunnstein@mailbox.org>
2023-05-01 10:15:19 +0200

    Added latest description

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ Explore github actions
 
 This repository is a test repository to explore github actions for automation of
 python packages.
+
+The following actions have been implemented:
+
+- Publish package to TestPyPI on push.
--- exgac/__init__.py
+++ exgac/__init__.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
-__version__ = "v0.1"
+__version__ = "v1.0"
 
 
 def hello_world():
     print("Hello world!")
+
+def now_published_on_push():
+    print("This package is published to TestPyPI on push to GitHUB")
